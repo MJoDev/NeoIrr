dd7cc24f207ba63d2cafe228c715cc3b05dc00e2
Moisés David Jiménez Ortiz <143739485+MJoDev@users.noreply.github.com>
2024-06-15 15:40:38 -0400

    SHARE PAGE

--- src/app/save/page.tsx
+++ src/app/save/page.tsx
@@ -6,7 +6,7 @@ import BackButton from '../components/BackButton/BackButton';
 const SavePage = () => {
   const [data, setData] = useState<any>(null);
   const [id, setId] = useState('');
-  const [description, setDescription] = useState('');
+  const [date, setDate] = useState('');
   const router = useRouter();
 
   useEffect(() => {
@@ -21,23 +21,57 @@ const SavePage = () => {
       const newRecord = {
         ...data,
         id,
-        description,
+        date,
       };
       const existingRecords = JSON.parse(localStorage.getItem('records') || '[]');
       existingRecords.push(newRecord);
       localStorage.setItem('records', JSON.stringify(existingRecords));
       localStorage.removeItem('currentData');
       alert('Data saved successfully!');
-      router.push('/records');
+      router.push('/record');
     }
   };
+
+  const handleShareClick = () => {
+    if (id && date) {
+      const shareData = {
+        ...data,
+        id,
+        date,
+      };
+      localStorage.setItem('shareData', JSON.stringify(shareData));
+      router.push('/share');
+    }
+  };
+  const isShareButtonDisabled = !id || !date;
+
 
   return (
         <div className="flex flex-col h-screen justify-between"> 
             <div className="mx-4">
-                <div className="bg-black text-white text-4xl text-center font-bold mb-10 mt-10">
-                    SAVE
+                <div className="bg-black text-white text-4xl text-center font-bold mb-5 mt-10">
+                    REC
                 </div> 
+                <div className='grid grid-cols-2 gap-2'>
+                    <button className="bg-gray-400 text-white text-4xl text-center font-bold">
+                        GENERAL
+                    </button>
+                    <button className="bg-gray-400 text-white text-4xl text-center font-bold">
+                        MATRIX
+                    </button>  
+                </div>
+                {!data && (
+                  <div className='grid mt-10'>
+                      <div className='mt-5'>
+                          <label className="block mb-2 text-md font-medium text-gray-900 ">DD/MM/YYY</label>
+                          <input type="date" placeholder="DD/MM/YYY" value={id} onChange={(e) => setDate(e.target.value)} className="block w-full p-5 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500"/>
+                      </div>
+                      <div className='mt-5'>
+                          <label className="block mb-2 text-md font-medium text-gray-900">TAG / SN / ID</label>
+                          <input type="text" placeholder="TAG / SN / ID" value={id} onChange={(e) => setId(e.target.value)} className="block w-full p-5 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 "/>
+                      </div>
+                 </div>
+                )}
                 
             </div>
             <div>
@@ -45,8 +79,8 @@ const SavePage = () => {
             </div>
             <div className="ml-2 mx-2"> 
                 <div className='grid grid-cols-2'>
-                    <button  className="rounded-full border border-blue-500 flex justify-center items-center mb-10 text-2xl gap-1 py-4 px-4 hover:scale-105 transition ml-2 mx-2">SAVE</button>
-                    <button  className="rounded-full border border-blue-800 flex justify-center items-center mb-10 text-2xl gap-1 py-4 px-4 hover:scale-105 transition ml-2 mx-2">SHARE</button>
+                    <button onClick={saveData} className="rounded-full border border-blue-500 flex justify-center items-center mb-10 text-2xl gap-1 py-4 px-4 hover:scale-105 transition ml-2 mx-2">SAVE</button>
+                    <button  onClick={handleShareClick} disabled={isShareButtonDisabled} className="rounded-full border border-blue-800 flex justify-center items-center mb-10 text-2xl gap-1 py-4 px-4 hover:scale-105 transition ml-2 mx-2">SHARE</button>
                 </div>
                 <BackButton/>
             </div>
---
+++ src/app/share/page.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import BackButton from '../components/BackButton/BackButton';
+import SpecialButton from '../components/SpecialButton/SpecialButton';
+import Link from 'next/link';
+
+const SharePage = () => {
+  const [data, setData] = useState<any>(null);
+  const router = useRouter();
+
+  useEffect(() => {
+    const storedData = localStorage.getItem('shareData');
+    if (storedData) {
+      setData(JSON.parse(storedData));
+    } else {
+      //router.push('/save');
+    }
+  }, [router]);
+
+  const handleShare = (method: 'email' | 'whatsapp') => {
+    if (data) {
+        const dataString = `ID: ${data.id}\nDescription: ${data.description}\nDate: ${data.date}\n\nProximity Data:\n${data.proximityData.join(', ')}\n\nLight Intensity Data:\n${data.lightData.join(', ')}`;
+        const encodedDataString = encodeURIComponent(dataString);
+      if (method === 'email') {
+        window.location.href = `mailto:?subject=Sensor Data&body=${encodedDataString}`;
+      } else if (method === 'whatsapp') {
+        window.location.href = `https://wa.me/?text=${encodedDataString}`;
+      }
+    }
+  };
+
+  return (
+    <div className="flex flex-col h-screen justify-between"> 
+            <div className="mx-4">
+                <div className="bg-black text-white text-4xl text-center font-bold mb-5 mt-10">
+                    SHARE
+                </div>  
+                <div className='grid mt-40'>
+                    <button  onClick={() => handleShare('email')} className="rounded-full border border-red-500 flex justify-center items-center mb-10 text-2xl gap-1 py-4 px-4 hover:scale-105 transition ml-2 mx-2">MAIL</button>
+                    <button  onClick={() => handleShare('whatsapp')} className="rounded-full border border-green-500 flex justify-center items-center mb-10 text-2xl gap-1 py-4 px-4 hover:scale-105 transition ml-2 mx-2">WHATSAPP</button>
+                </div>
+                
+            </div>
+            <div className="ml-2 mx-2"> 
+                <Link href={'/save'} className="rounded-full border border-black flex justify-center items-center mb-10 text-2xl gap-1 py-4 px-4 hover:scale-105 transition ml-5 mx-5">
+                    BACK
+                </Link>
+            </div>
+        </div>
+  );
+}
+
+export default SharePage;
