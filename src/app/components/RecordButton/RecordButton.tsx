6f86e6f06433b92843fc6ba328c2ef6c029df9de
Moisés David Jiménez Ortiz <143739485+MJoDev@users.noreply.github.com>
2024-06-21 13:23:58 -0400

    delete

--- src/app/components/RecordButton/RecordButton.tsx
+++
@@ -1,9 +0,0 @@
-import Link from "next/link";
-
-export default function RecordButton() {
-    return (
-        <Link href={'/record'} className="rounded-full border border-blue-500 flex justify-center items-center mb-10 text-2xl gap-1 py-4 px-4 hover:scale-105 transition mx-5 ml-5">
-            REC
-        </Link>
-    )
-}
