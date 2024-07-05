2607c4f4506f63c42d8caeeec052570011e15674
Moisés David Jiménez Ortiz <143739485+MJoDev@users.noreply.github.com>
2024-07-05 12:11:44 -0400

    Separación

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <footer className="flex w-full justify-center py-5 mx-auto mb-10 footerPage">
             <div className="grid grid-cols-1 justify-center text-center">
-                <Link className="flex justify-center" href="/"><img className="w-auto h-20 size-20" src="./neoirr.png" alt="NeoIrr"/></Link>
+                <Link className="flex justify-center mb-3" href="/"><img className="w-auto h-20 size-20" src="./neoirr.png" alt="NeoIrr"/></Link>
                 <div className="text-sm text-center font-bold">BASED ON IEC 60601-2-50 & AAP STANDARDS</div>
                 <div className="text-sm text-center">UPDATED 2024</div>
             </div>      
