69048eb1cbec0eb3d907d2f91907d5fc055bca62
Moisés David Jiménez Ortiz <143739485+MJoDev@users.noreply.github.com>
2025-01-13 15:01:20 -0400

    Update Matrix.tsx

--- src/app/components/Matrix/Matrix.tsx
+++ src/app/components/Matrix/Matrix.tsx
@@ -110,7 +110,7 @@ export default function Matrix(MatrixProps: MatrixProps) {
             <div>
                 <div className="flex items-center justify-center h-21 w-21 py-4 bg-gray-100 border-2 border-gray-300 rounded-lg">
                     <div className="text-center">
-                        <div className="text-3xl font-bold text-gray-700" style={getLightStyle38(MatrixProps.text[1])}>{MatrixProps.text[7]}</div>
+                        <div className="text-3xl font-bold text-gray-700" style={getLightStyle38(MatrixProps.text[7])}>{MatrixProps.text[7]}</div>
                     </div>
                 </div>
                 <div className="text-md text-black text-center mt-2" >REF:38 - 59 [uW/cm2/nm]</div>
