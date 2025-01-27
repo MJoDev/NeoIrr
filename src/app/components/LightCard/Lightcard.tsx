6059eda99b576fdf64aae8730f2ddc8b8587600b
Moisés David Jiménez Ortiz <143739485+MJoDev@users.noreply.github.com>
2025-01-27 11:35:31 -0400

    Update Lightcard.tsx

--- src/app/components/LightCard/Lightcard.tsx
+++ src/app/components/LightCard/Lightcard.tsx
@@ -8,8 +8,8 @@ interface LightCardProps {
 
 export default function LightCard ({ borderSelected, style, value, reference, onClick }: LightCardProps)  {
   return (
-    <div onClick={onClick}>
-      <div className="flex items-center justify-center h-21 w-21 py-4 bg-gray-100 border-2 border-gray-300 rounded-lg" style={borderSelected}>
+    <div>
+      <div className="flex items-center justify-center h-21 w-21 py-4 bg-gray-100 border-2 border-gray-300 rounded-lg" onClick={onClick} style={borderSelected}>
         <div className="text-center">
           <div className="text-3xl font-bold text-gray-700" style={style}>{value}</div>
         </div>
