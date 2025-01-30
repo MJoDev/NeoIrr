e7e6a729837abf9f778369644ace80bdce042aeb
defedeleboria <fedeledelicia@gmail.com>
2025-01-29 21:18:01 -0300

    Redimensión

--- src/app/components/StatusFailed/StatusFailed.tsx
+++ src/app/components/StatusFailed/StatusFailed.tsx
@@ -1,9 +1,9 @@
 export default function StatusFailed() {
     return (
       <div className="flex items-center justify-center mb-10">
-        <div className="bg-red-500 text-white px-8 py-6 rounded-md shadow-lg">
+        <div className="bg-red-500 text-white px-12 py-12 rounded-lg shadow-lg">
           <div className="flex flex-col items-center space-y-4">
-            <h2 className="text-2xl font-bold">Status FAILED</h2>
+            <h2 className="text-lg font-medium">Status FAILED</h2>
             <p>Try again please.</p>
           </div>
         </div>
